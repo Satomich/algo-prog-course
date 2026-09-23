2a5b58212f909410e69a799b0c2fd9ede2e22c54
Satomich <kolostimosha@gmail.com>
2026-09-23 14:27:32 +0300

    Lab01 task 2: temperature converter

---
+++ TempConverter/Program.cs
@@ -0,0 +1,9 @@
+﻿const int A = 9/5;
+const int B = 32;
+const double C = 273.15;
+const double celsius = 13.5;
+Console.WriteLine($"Шкала цельсия: {celsius}");
+double Farengeit = celsius  * A + B;
+Console.WriteLine($"Расчёт по шкале Фаренгейта: {Farengeit}");
+double Kelvin = celsius + C;
+Console.WriteLine($"Расчёт по шкале Кельвина: {Kelvin}");
